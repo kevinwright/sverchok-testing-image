2d7947e233b7dee30ccf164e419c7f289e91c3a1
Kevin Wright <kev.lee.wright@gmail.com>
2022-03-31 20:12:46 +0100

    improved entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/sh -l
 
+echo "Installing Addon"
+blender -b --python /bpy_activate_addon.py --python-exit-code 1 -- $@
+
+echo "Installing userprefs"
 mkdir -p ${BLENDER_CONFIG_PATH}
 ln -s ./tests/references/userpref.blend ${BLENDER_CONFIG_PATH}
-blender -b --python /bpy_activate_addon.py --python-exit-code 1 -- $@
-ls /github/home/.config/blender/2.93/scripts/addons
+
+echo "Launching tests"
 blender -b --addons sverchok --python utils/testing.py --python-exit-code 1 -- $@
+
 time=$(date)
 echo ::set-output name=time::$time
