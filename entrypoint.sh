b74738182ac44027ddc487ea33d4c09365397a75
Kevin Wright <kev.lee.wright@gmail.com>
2022-03-31 19:24:00 +0100

    list addons after install

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 mkdir -p ${BLENDER_CONFIG_PATH}
 ln -s ./tests/references/userpref.blend ${BLENDER_CONFIG_PATH}
 blender -b --python /bpy_activate_addon.py --python-exit-code 1 -- $@
+ls /github/home/.config/blender/2.93/scripts/addons
 blender -b --addons sverchok --python utils/testing.py --python-exit-code 1 -- $@
 time=$(date)
 echo ::set-output name=time::$time
