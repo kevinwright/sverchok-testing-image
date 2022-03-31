bb34a6aa93907d747ee6f705f5fc797d1723cf80
Kevin Wright <kev.lee.wright@gmail.com>
2022-03-31 21:23:55 +0100

    swim, swim, swim

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,14 @@
 
 echo $HOME
 export BLENDER_VERSION=$(blender -b --version | head -n 1 | cut -d' ' -f2 | cut -d'.' -f 1-2)
-set BLENDER_CONFIG_PATH=/github/home/.config/blender/${BLENDER_VERSION}/config
+set BLENDER_CONFIG_PATH="/github/home/.config/blender/${BLENDER_VERSION}/config"
+echo "${BLENDER_CONFIG_PATH}"
 
 echo "Installing Addon"
 blender -b --python /bpy_activate_addon.py --python-exit-code 1 -- $@
 
 echo "Installing userprefs"
-mkdir -p ${BLENDER_CONFIG_PATH}
+mkdir -p "${BLENDER_CONFIG_PATH}"
 ln -s ./tests/references/userpref.blend ${BLENDER_CONFIG_PATH}
 
 echo "Launching tests"
