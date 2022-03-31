97070e0c9711d40789ff2c9818c652bb45258b67
Kevin Wright <kev.lee.wright@gmail.com>
2022-03-31 20:46:37 +0100

    playing with config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -l
 
+echo $HOME
+export BLENDER_VERSION=$(blender -b --version | head -n 1 | cut -d' ' -f2 | cut -d'.' -f 1-2)
+set BLENDER_CONFIG_PATH=/github/home/.config/blender/${BLENDER_VERSION}/config
+
 echo "Installing Addon"
 blender -b --python /bpy_activate_addon.py --python-exit-code 1 -- $@
 
