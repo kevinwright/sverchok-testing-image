0d7fe273cef5b2afd844e7316bd7965bc708adba
Kevin Wright <kev.lee.wright@gmail.com>
2022-03-31 23:23:30 +0100

    Move logic out of container build

--- bpy_activate_addon.py
+++
@@ -1,6 +0,0 @@
-import bpy
-
-# Register the addon and enable it
-bpy.ops.preferences.addon_install(filepath='./sverchok.zip')
-bpy.ops.preferences.addon_enable(module='sverchok')
-bpy.ops.wm.save_userpref()
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,33 +1,3 @@
 #!/bin/sh -l
 
-export BLENDER_VERSION=$(blender -b --version | head -n 1 | cut -d' ' -f2 | cut -d'.' -f 1-2)
-export BLENDER_ADDON_PATH="$HOME/.config/blender/${BLENDER_VERSION}/scripts/addons"
-export BLENDER_CONFIG_PATH="$HOME/.config/blender/${BLENDER_VERSION}/config"
-
-echo "HOME = $HOME"
-echo "BLENDER_ADDON_PATH = ${BLENDER_ADDON_PATH}"
-echo "BLENDER_CONFIG_PATH = ${BLENDER_CONFIG_PATH}"
-
-echo ""
-echo "Installing Addon"
-echo "================"
-
-blender -b --python /bpy_activate_addon.py --python-exit-code 1 -- $@
-
-# echo ""
-# echo "Installing userprefs"
-# echo "===================="
-
-# mkdir -p "${BLENDER_CONFIG_PATH}"
-# ln -s ./tests/references/userpref.blend ${BLENDER_CONFIG_PATH}
-
-# we enabled it in python... this probably isn't needed
-
-echo ""
-echo "Launching tests"
-echo "==============="
-
-blender -b --python "$BLENDER_ADDON_PATH/sverchok/utils/testing.py" --python-exit-code 1 -- $@
-
-ls "${BLENDER_ADDON_PATH}/tests/*.log"
-ls "${BLENDER_ADDON_PATH}/tests/log_files"
+$HOME/ci-utils/start-ci-test.sh
