b3e6257b102fc6c07de7836d98c5081fe04cf084
Kevin Wright <kev.lee.wright@gmail.com>
2022-03-31 18:33:40 +0100

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "Hello $1"
 ln -s ./tests/references/userpref.blend ${BLENDER_PATH}/config/
-blender -b --python ~/bpy_activate_addon.py --python-exit-code 1 -- $@
+blender -b --python /bpy_activate_addon.py --python-exit-code 1 -- $@
 blender -b --addons sverchok --python utils/testing.py --python-exit-code 1 -- $@
 time=$(date)
 echo ::set-output name=time::$time
