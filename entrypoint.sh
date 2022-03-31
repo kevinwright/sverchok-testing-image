63c096163f8eee68c71791625a01c90321f4dcad
Kevin Wright <kev.lee.wright@gmail.com>
2022-03-31 14:36:09 +0100

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/sh -l
 
 echo "Hello $1"
-echo "${BLENDER_PATH}" 
-cd /github/workspace
-ln -s ./tests/references/userpref.blend ~/.config/blender/${BLENDER_VERSION}/config/
-blender -background --python ./bpy_activate_addon.py --python-exit-code 1 -- $@
-blender -background --addons sverchok --python ./utils/testing.py --python-exit-code 1 -- $@
+ln -s ./tests/references/userpref.blend ${BLENDER_PATH}/config/
+blender -b --python ./bpy_activate_addon.py --python-exit-code 1 -- $@
+blender -b --addons sverchok --python ./utils/testing.py --python-exit-code 1 -- $@
 time=$(date)
 echo ::set-output name=time::$time
