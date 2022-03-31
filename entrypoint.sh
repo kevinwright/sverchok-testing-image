fbd869dec58094e126bd0875b2718620066c1222
Kevin Wright <kev.lee.wright@gmail.com>
2022-03-31 19:20:16 +0100

    keep on swimming

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -l
 
-echo "$PWD"
-echo "Hello $1"
+mkdir -p ${BLENDER_CONFIG_PATH}
 ln -s ./tests/references/userpref.blend ${BLENDER_CONFIG_PATH}
 blender -b --python /bpy_activate_addon.py --python-exit-code 1 -- $@
 blender -b --addons sverchok --python utils/testing.py --python-exit-code 1 -- $@
